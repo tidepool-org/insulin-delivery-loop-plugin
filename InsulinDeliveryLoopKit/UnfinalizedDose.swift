bff22cb4ed7b88eb624edcbbed983ec20c90d778
Pete Schwamb <pete@schwamb.net>
2026-02-12 18:17:47 -0600

    Fix insulin type tracking; was being lost during conversion to DoseEntry

--- InsulinDeliveryLoopKit/UnfinalizedDose.swift
+++ InsulinDeliveryLoopKit/UnfinalizedDose.swift
@@ -320,13 +320,13 @@ extension DoseEntry {
     init (_ dose: UnfinalizedDose, at date: Date, isFinalized: Bool = false) {
         switch dose.doseType {
         case .bolus:
-            self = DoseEntry(type: .bolus, startDate: dose.startTime, endDate: dose.endTime, value: dose.programmedUnits ?? dose.units, unit: .units, decisionId: dose.decisionId, deliveredUnits: dose.finalizedUnits(at: date), automatic: dose.automatic, isMutable: !isFinalized, wasProgrammedByPumpUI: false)
+            self = DoseEntry(type: .bolus, startDate: dose.startTime, endDate: dose.endTime, value: dose.programmedUnits ?? dose.units, unit: .units, decisionId: dose.decisionId, deliveredUnits: dose.finalizedUnits(at: date), insulinType: dose.insulinType, automatic: dose.automatic, isMutable: !isFinalized, wasProgrammedByPumpUI: false)
         case .tempBasal:
-            self = DoseEntry(type: .tempBasal, startDate: dose.startTime, endDate: dose.endTime, value: dose.programmedRate ?? dose.rate, unit: .unitsPerHour, decisionId: dose.decisionId, deliveredUnits: dose.finalizedUnits(at: date), automatic: dose.automatic, isMutable: !isFinalized)
+            self = DoseEntry(type: .tempBasal, startDate: dose.startTime, endDate: dose.endTime, value: dose.programmedRate ?? dose.rate, unit: .unitsPerHour, decisionId: dose.decisionId, deliveredUnits: dose.finalizedUnits(at: date), insulinType: dose.insulinType, automatic: dose.automatic, isMutable: !isFinalized)
         case .suspend:
             self = DoseEntry(suspendDate: dose.startTime, automatic: dose.automatic)
         case .resume:
-            self = DoseEntry(resumeDate: dose.startTime, automatic: dose.automatic)
+            self = DoseEntry(resumeDate: dose.startTime, insulinType: dose.insulinType, automatic: dose.automatic)
         }
     }
 }
